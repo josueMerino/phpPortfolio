99c48e4f3c9a9eb975f9371ee07f95713217055d
josue.merino <josue.merino@alumnos.uneatlantico.es>
2020-03-18 14:16:06 +0100

    DB added

--- cursophp.sql
+++ cursophp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-03-2020 a las 11:29:12
+-- Tiempo de generación: 18-03-2020 a las 14:15:02
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.3
 
@@ -34,6 +34,7 @@ CREATE TABLE `jobs` (
   `description` text NOT NULL,
   `visible` tinyint(1) NOT NULL DEFAULT 1,
   `months` int(11) NOT NULL,
+  `image` blob NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -42,10 +43,8 @@ CREATE TABLE `jobs` (
 -- Volcado de datos para la tabla `jobs`
 --
 
-INSERT INTO `jobs` (`id`, `title`, `description`, `visible`, `months`, `created_at`, `updated_at`) VALUES
-(1, 'Node Dev', 'Authentic', 1, 18, '2020-03-15 20:42:12', '2020-03-15 20:42:12'),
-(2, 'MIRELLONA', 'hola mundo', 1, 12, '2020-03-16 01:46:27', '2020-03-16 01:46:27'),
-(3, 'One piece', 'Desc1', 1, 12, '2020-03-16 11:28:08', '2020-03-16 11:28:08');
+INSERT INTO `jobs` (`id`, `title`, `description`, `visible`, `months`, `image`, `created_at`, `updated_at`) VALUES
+(1, 'Python Dev', 'Authentic', 1, 13, 0x75706c6f6164732f707974686f6e2e706e67, '2020-03-18 14:04:55', '2020-03-18 14:04:55');
 
 -- --------------------------------------------------------
 
@@ -74,6 +73,28 @@ INSERT INTO `projects` (`id`, `title`, `description`, `created_at`, `updated_at`
 (6, 'New project', 'New information', '2020-03-16 10:21:55', '2020-03-16 10:21:55'),
 (7, 'New project 1.1', 'New information 1.2', '2020-03-16 10:50:20', '2020-03-16 10:50:20');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `email` text NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'raptorjm', 'jfk@email.com', '$2y$10$Kt2tqLdhJ2tpX3o5IiQyYeQzoZdOLy73Zt2YR3BaRgPm7wN6C1LWG', '2020-03-18 10:49:59', '2020-03-18 10:49:59');
+
 --
 -- Índices para tablas volcadas
 --
@@ -91,6 +112,12 @@ ALTER TABLE `projects`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -98,13 +125,19 @@ ALTER TABLE `projects`
 -- AUTO_INCREMENT de la tabla `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `projects`
 --
 ALTER TABLE `projects`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
