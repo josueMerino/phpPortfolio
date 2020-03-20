311b459ad7625a8b4dd9d0eb1f892d9aff61abcc
josue.merino <josue.merino@alumnos.uneatlantico.es>
2020-03-20 13:54:36 +0100

    Fixing new problems

--- app/Controllers/Database.php
+++ app/Controllers/Database.php
@@ -12,7 +12,7 @@ class Database {
         $capsule = new Capsule;
 
     $capsule->addConnection([
-        'driver'    => 'mysql',
+        'driver'    => getenv('DB_DRIVER'),
         'host'      => getenv('DB_HOST'),
         'database'  => getenv('DB_NAME'),
         'username'  => getenv('DB_USER'),
@@ -20,6 +20,7 @@ class Database {
         'charset'   => 'utf8',
         'collation' => 'utf8_unicode_ci',
         'prefix'    => '',
+        'port'      => getenv('DB_PORT')
     ]);
     
     // Make this Capsule instance available globally via static methods... (optional)
